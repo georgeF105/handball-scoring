57ce38cf6fcec1a9d68df5655b0d982ee5055abc
george francis <georgefrancis105@gmail.com>
2016-08-16 14:33:54 +1200

    Make games route

--- app/components/routes.js
+++ app/components/routes.js
@@ -4,6 +4,7 @@ import { Router, Route, hashHistory, IndexRoute } from 'react-router'
 import App from './App'
 import Home from './Home'
 import NewGameContainer from './containers/NewGameContainer'
+import Games from './Games'
 
 export default (
   <Router history={hashHistory}>
@@ -11,6 +12,7 @@ export default (
       <IndexRoute component={Home} />
       <Route path='/game/new' component={NewGameContainer} />
       <Route path='/game/:id' component={Home} />
+      <Route path='/game/' component={Games} />
     </Route>
   </Router>
 )
