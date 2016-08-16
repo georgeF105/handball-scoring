0d4f4e1bd9fac2ff8ebe9431572928d278cd8dcb
george francis <georgefrancis105@gmail.com>
2016-08-16 15:14:47 +1200

    make game link only visable to game owner

--- app/components/Games.js
+++ app/components/Games.js
@@ -3,10 +3,12 @@ import React from 'react'
 import GameCard from './GameCard'
 
 export default (props) => {
+  const userId = props.userId
   const games = []
   for(let key in props.games) {
     const game = props.games[key]
     game.gameKey = key
+    game.ownedByCurrentUser = game.owner_id === userId
     games.push(game)
   }
   const fetchingGames = props.fetchingGames
