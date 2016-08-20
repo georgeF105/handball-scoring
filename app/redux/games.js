e396351ac367574091e43184f4e0d5b66c5be14a
george francis <georgefrancis105@gmail.com>
2016-08-20 22:12:35 +1200

    Remove unused code

--- app/redux/games.js
+++ app/redux/games.js
@@ -3,21 +3,12 @@ import { fromJS } from 'immutable'
 import * as gamesActions from './gamesActions'
 
 const INITAL_STATE = fromJS({
-  game: {},
-  fetchingGame: false,
   games: {},
   fetchingGames: false
 })
 
 export default (state = INITAL_STATE, action) => {
   switch (action.type) {
-    case gamesActions.REQUEST_GAME:
-      return state.setIn(['fetchingGame'], true)
-    case gamesActions.RECEIVE_GAME:
-      console.log('RECEIVE_GAME', action.gameObj)
-      return state.setIn(['game'], fromJS(action.gameObj)).set('fetchingGame', false)
-    case gamesActions.SAVED_GAME:
-      return state.setIn(['game'], fromJS(action.gameObj)).set('fetchingGame', false)
     case gamesActions.REQUEST_GAMES:
       return state.setIn(['fetchingGames'], true)
     case gamesActions.RECEIVE_GAMES:
--- app/redux/gamesActions.js
+++ app/redux/gamesActions.js
@@ -1,59 +1,12 @@
 import { saveGame, getGames, getGame, getTeam, updateGameChild, appendPlayersToGame, pushEvent, removeGame } from '../../lib/gamesUtils'
 import { hashHistory } from 'react-router'
 
-export const SAVED_GAME = 'SAVED_GAME'
-export const REQUEST_GAME = 'REQUEST_GAME'
-export const RECEIVE_GAME = 'RECEIVE_GAME'
 export const REQUEST_GAMES = 'REQUEST_GAMES'
 export const RECEIVE_GAMES = 'RECEIVE_GAMES'
-
-export function requestGame () {
-  return {
-    type: REQUEST_GAME
-  }
-}
-
-export function reciveGame (gameObj) {
-  return {
-    type: RECEIVE_GAME,
-    gameObj: gameObj,
-    receivedAt: Date.now()
-  }
-}
-
-export function fetchGame (gameKey) {
-  return (dispatch) => {
-    dispatch(requestGame())
-    console.log('fetching game HERE', gameKey)
-    let gameObj = {}
-    getGame(gameKey)
-      .then(game => {
-        gameObj = game
-        gameObj.gameKey = gameKey
-        return getTeam(game.home_team)
-      })
-      .then(homeTeam => {
-        const teamKey = gameObj.home_team
-        gameObj.home_team = homeTeam
-        gameObj.home_team.key = teamKey
-        return getTeam(gameObj.away_team)
-      })
-      .then(awayTeam => {
-        const teamKey = gameObj.away_team
-        gameObj.away_team = awayTeam
-        gameObj.away_team.key = teamKey
-        dispatch(reciveGame(gameObj))
-      })
-      .catch(err => {
-        console.error(err)
-      })
-  }
-}
 
 export function initializeGame (gameKey) {
   return (dispatch) => {
     appendPlayersToGame(gameKey)
-    // updateGameChild(gameKey, 'status_initialized', true)
   }
 }
 
@@ -61,7 +14,6 @@ export function submitGame (gameObj) {
   return (dispatch) => {
     const gameKey = saveGame(gameObj)
     gameObj.gameKey = gameKey
-    // dispatch(savedGame(gameObj))
     hashHistory.push('/')
   }
 }
@@ -69,13 +21,6 @@ export function submitGame (gameObj) {
 export function deleteGame (gameKey) {
   return (dispatch) => {
     removeGame(gameKey)
-  }
-}
-
-export function savedGame (gameObj) {
-  return {
-    type: SAVED_GAME,
-    gameObj: gameObj
   }
 }
 
