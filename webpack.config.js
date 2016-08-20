3f3ec5ffdece334f5f43061176cb7149b9b3f987
george francis <georgefrancis105@gmail.com>
2016-08-20 21:50:36 +1200

    Attach NODE_ENV to bundle using webpack DefinePlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack')
 
 module.exports = {
   entry: './index.js',
@@ -7,6 +8,13 @@ module.exports = {
     filename: 'bundle.js',
     publicPath: '/'
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    })
+  ],
   module: {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react&presets[]=stage-0' }
