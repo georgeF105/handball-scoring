8dd09b1fc24a49294474f09616c5bf589d2fcfb3
george francis <georgefrancis105@gmail.com>
2016-08-27 17:43:45 +1200

    Make simpleFilter function

--- tests/filter.js
+++ tests/filter.js
@@ -1,18 +1,27 @@
 import test from 'tape'
 
-import { simpleFilter } from '../lib/filter'
+import { convertToArray, simpleFilter } from '../lib/filter'
 
 import testDB from './test-db.json'
 
 test('Filter games by sex', (t) => {
-  const games = testDB.games
-  const expectedMaleResults = 5
-  const expectedFemaleResults = 3
-
+  const games = convertToArray(testDB.games)
+  const expectedMaleResults = 4
+  const expectedFemaleResults = 2
   const filteredMaleGames = simpleFilter(games, {gender: 'male'})
   const filteredFemaleGames = simpleFilter(games, {gender: 'female'})
 
   t.equal(filteredMaleGames.length, expectedMaleResults, 'filter by male game got the expected amount of results')
-  t.equal(filteredFemaleGames.length, expectedMaleResults, 'filter by male game got the expected amount of results')
+  t.equal(filteredFemaleGames.length, expectedFemaleResults, 'filter by male game got the expected amount of results')
+  t.end()
+})
+
+test('Filter games nothing', (t) => {
+  const games = convertToArray(testDB.games)
+  const expectedResults = games.length
+
+  const filteredResults = simpleFilter(games, {})
+
+  t.equal(filteredResults.length, expectedResults, 'filter nothing got the expected amount of results')
   t.end()
 })
