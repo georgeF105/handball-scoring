e21b184914ef67cb164e7304f9a2acabda78de68
george francis <georgefrancis105@gmail.com>
2016-08-27 15:53:26 +1200

    Style game card, make gameCard.scss

--- app/components/GameCard.js
+++ app/components/GameCard.js
@@ -8,12 +8,29 @@ export default (props) => {
   const deleteGame = props.deleteGame
   return (
     <div className='card game-card'>
-      <h5>{homeTeam}</h5>
-      <h5>VS</h5>
-      <h5>{awayTeam}</h5>
-      <p>{game.date} - {game.time}</p>
-      <Link to={`/game/${game.gameKey}/score`}>Score Game</Link>
-      <i className='fa fa-trash delete-game-icon' name={game.gameKey} onClick={deleteGame} />
+      <div className='card-heading'>
+        <h5>{game.venue}</h5>
+        <h5>{game.date} - {game.time}</h5>
+        <i className='fa fa-share icon' />
+      </div>
+      <div className='card-content'>
+        <h4>Not Started</h4>
+        <div className='card-score'>
+          <div className='score'>00</div>
+          <h4>1'st</h4>
+          <div className='score'>00</div>
+        </div>
+        <div className='game-teams'>
+          <h5 className='team-name'>{homeTeam}</h5>
+          <h5>VS</h5>
+          <h5 className='team-name'>{awayTeam}</h5>
+        </div>
+      </div>
+      <div className='card-options'>
+        <i className='fa fa-cog icon' />
+        <Link to={`/game/${game.gameKey}/score`}>Score Game</Link>
+        <i className='fa fa-trash delete-game-icon' name={game.gameKey} onClick={deleteGame} />
+      </div>
     </div>
   )
 }
