eff01f7e247cd75d376e3a7b89ca137afac7655c
george francis <georgefrancis105@gmail.com>
2016-08-27 14:01:53 +1200

    Fix linting

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -2,11 +2,6 @@ import React from 'react'
 import { Link } from 'react-router'
 
 class NavBar extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = { optionsView: false }
-  }
-
   toggleOptions = () => {
     this.setState({optionsView: !this.state.optionsView})
   }
@@ -16,20 +11,19 @@ class NavBar extends React.Component {
     const loggedIn = this.props.loggedIn
     const logIn = this.props.logIn
     const logOut = this.props.logOut
-    const optionsView = this.state.optionsView
     return (
       <nav className='navbar navbar-default navbar-fixed-top'>
         <div className='container'>
           <div className='navbar-header'>
-            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="true" aria-controls="navbar">
-              <span className="sr-only">Toggle navigation</span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
+            <button type='button' className='navbar-toggle' data-toggle='collapse' data-target='#navbar' aria-expanded='true' aria-controls='navbar'>
+              <span className='sr-only'>Toggle navigation</span>
+              <span className='icon-bar' />
+              <span className='icon-bar' />
+              <span className='icon-bar' />
             </button>
             <Link className='navbar-brand' to='/'>Handball Scoring</Link>
           </div>
-          <div id='navbar' className='navbar-collapse collapse in' aria-expanded="true">
+          <div id='navbar' className='navbar-collapse collapse in' aria-expanded='true'>
             <ul className='nav navbar-nav navbar-right'>
               <li><p className='navbar-text'>{userName}</p></li>
               <li><Link className='' activeClassName='active' to='game/new'>New Game</Link></li>
