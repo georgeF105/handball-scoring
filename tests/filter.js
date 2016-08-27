4929aa041e831d654630e26c1f84700bc9f36734
george francis <georgefrancis105@gmail.com>
2016-08-27 17:03:12 +1200

    Make simple filter test

--- tests/filter.js
+++ tests/filter.js
@@ -1,4 +1,18 @@
 import test from 'tape'
-import testDB from '/test-db.json'
 
-console.log('games', testDB.games)
+import { simpleFilter } from '../lib/filter'
+
+import testDB from './test-db.json'
+
+test('Filter games by sex', (t) => {
+  const games = testDB.games
+  const expectedMaleResults = 5
+  const expectedFemaleResults = 3
+
+  const filteredMaleGames = simpleFilter(games, {gender: 'male'})
+  const filteredFemaleGames = simpleFilter(games, {gender: 'female'})
+
+  t.equal(filteredMaleGames.length, expectedMaleResults, 'filter by male game got the expected amount of results')
+  t.equal(filteredFemaleGames.length, expectedMaleResults, 'filter by male game got the expected amount of results')
+  t.end()
+})
