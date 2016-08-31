5d3540ba14e8db6e895a6b28fcaa45ad76c7a1ab
george francis <georgefrancis105@gmail.com>
2016-09-01 09:19:51 +1200

    Fix overview page col headers

--- app/components/Overview.js
+++ app/components/Overview.js
@@ -5,21 +5,17 @@ import GamesContainer from './containers/GamesContainer'
 export default (props) => {
   return (
     <div className='container overview'>
-      <div className='page-header'>
-        <div className='row'>
-          <div className='col-md-6'>
-            <h3>Mens</h3>
-          </div>
-          <div className='col-md-6'>
-            <h3>Womans</h3>
-          </div>
-        </div>
-      </div>
       <div className='row'>
         <div className='col-md-6'>
+          <div className='page-header'>
+            <h3>Mens</h3>
+          </div>
           <GamesContainer filter={{gender: 'male'}} />
         </div>
         <div className='col-md-6'>
+          <div className='page-header'>
+            <h3>Womans</h3>
+          </div>
           <GamesContainer filter={{gender: 'female'}} />
         </div>
       </div>
