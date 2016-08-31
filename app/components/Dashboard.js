85d55a605973f004dd88e084962ba8039345a5ea
george francis <georgefrancis105@gmail.com>
2016-09-01 11:01:02 +1200

    Add basic search field

--- app/components/Dashboard.js
+++ app/components/Dashboard.js
@@ -7,9 +7,20 @@ import Players from './Players'
 import { removeGame } from '../../lib/gamesUtils'
 
 class Dashboard extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      searchField: ''
+    }
+  }
+
   deleteGame = (e) => {
     e.preventDefault()
     removeGame(e.target.getAttribute('name'))
+  }
+
+  updateSearchField = (e) => {
+    this.setState({searchField:e.target.value})
   }
   render () {
     const userId = this.props.userId
@@ -17,26 +28,28 @@ class Dashboard extends React.Component {
     const teams = this.props.teams || {}
     const players = this.props.players || {}
     const fetching = this.props.fetchingGames || this.props.fetchingTeams || this.props.fetchingPlayers
+    const searchField = this.state.searchField
     return (
       <div className='container content dashboard'>
+        <input type='text' placeholder='search' onChange={this.updateSearchField} />
         <div className='row'>
           <div className='col-md-4'>
             <div className='page-header'>
               <h3>Games</h3>
             </div>
-            {fetching ? <h3>Loading...</h3> : <Games userId={userId} games={games} teams={teams} deleteGame={this.deleteGame} isAdmin />}
+            {fetching ? <h3>Loading...</h3> : <Games userId={userId} games={games} teams={teams} deleteGame={this.deleteGame} isAdmin search={searchField} />}
           </div>
           <div className='col-md-4'>
             <div className='page-header'>
               <h3>Teams</h3>
             </div>
-            {fetching ? <h3>Loading...</h3> : <Teams teams={teams} />}
+            {fetching ? <h3>Loading...</h3> : <Teams teams={teams} search={searchField} />}
           </div>
           <div className='col-md-4'>
             <div className='page-header'>
               <h3>Players</h3>
             </div>
-            {fetching ? <h3>Loading...</h3> : <Players players={players} />}
+            {fetching ? <h3>Loading...</h3> : <Players players={players} search={searchField} />}
           </div>
         </div>
       </div>
